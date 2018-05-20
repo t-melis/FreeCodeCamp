ad77cae87258cc07f00a46ea1da43ac4b9bcb972
Thanos Melistas <thanos.melistas@gmail.com>
2018-05-20 19:15:14 +0300

    add factorialize a number

---
+++ Exercises/factorializeANumber.js
@@ -0,0 +1,11 @@
+
+// Factorializes a Number
+function factorialize(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  else{
+           return num * factorialize(num-1);
+  }
+}
+factorialize(5);
